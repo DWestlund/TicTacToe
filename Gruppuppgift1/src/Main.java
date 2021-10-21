4222c8464e7dc0c51d88750f2507f4092875b5ce
LindaHurtig <92912834+LindaHurtig@users.noreply.github.com>
2021-10-21 11:02:51 +0200

    test

--- Gruppuppgift1/src/Main.java
+++ Gruppuppgift1/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Test för första gruppuppgiften!");
 		System.out.println("Mitt andra commit är nu uppe");
-
+		System.out.println("Test push");
 	}
 
 }
