c9fe987ea417ab93e13840883862d5a7b5b394ec
David Westlund <david.westlund@yaelev.se>
2021-10-21 15:37:02 +0200

    Svar på Daniels kodrad

--- Gruppuppgift1/src/Main.java
+++ Gruppuppgift1/src/Main.java
@@ -5,7 +5,8 @@ public class Main {
 		System.out.println("Test för första gruppuppgiften!");
 		System.out.println("Mitt andra commit är nu uppe");
 		System.out.println("Test push");
-		System.out.println("Tror jag fått till det")
+		System.out.println("Tror jag fått till det");
+		System.out.println("Det funkar bra! Fick ned din kod! :)");
 	}
 
 }
