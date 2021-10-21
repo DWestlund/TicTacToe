66d438507ca148573486e140e7b209f9173d0175
David Westlund <david.westlund@yaelev.se>
2021-10-21 10:26:17 +0200

    Lagt till en ny System.out.println

--- Gruppuppgift1/src/Main.java
+++ Gruppuppgift1/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Test för första gruppuppgiften!");
+		System.out.println("Mitt andra commit är nu uppe");
 
 	}
 
