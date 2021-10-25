72836c62a56f7a29164e6b863907ee0a77f8b2e9
David Westlund <david.westlund@yaelev.se>
2021-10-25 14:18:31 +0200

    Rensat till 1 spelplan

--- Gruppuppgift1/src/com/grupp4a/tictactoe/Board.java
+++ Gruppuppgift1/src/com/grupp4a/tictactoe/Board.java
@@ -12,55 +12,13 @@ public class Board {
 		this.boardArray = boardArray;
 	}
 	
-	public static void main (String[] args) {
-		Board board = new Board();		
-//		board.editBoard(board);
-		
-		board.printBoard(board);
-		
-		board.printBoard2(board);
-		
-		board.printBoard3(board);
-	}
-	
 	
 	private void editBoard(Board board) {
 		board.boardArray[0][0] = 'X';
 	}
-	
-	
+		
 	private void printBoard(Board board) {
-		System.out.println();
-		
-		System.out.println(" -------------");		
-		for(int row = 0; row < board.boardArray.length; row++) {
-			
-			for(int col = 0; col < board.boardArray[row].length; col++) {
-				System.out.print(" | " + board.boardArray[row][col]);
-			}		
-			System.out.println(" |");
-			System.out.println(" -------------");
-		}		
-	}
-	
-	private void printBoard2(Board board) {
-		System.out.println();
-		
-		for(int row = 0; row < board.boardArray.length; row++) {
-			
-			for(int col = 0; col < board.boardArray[row].length; col++) {
-				System.out.print(" | " + board.boardArray[row][col]);
-			}		
-			System.out.println(" |");			
-			System.out.println((row != 2) ? " -------------" : "");
-		}
-		
-	}
-	
-	
-	
-	
-	private void printBoard3(Board board) {
+		System.out.println("  Spelplan");
 		for(int row = 0; row < board.boardArray.length; row++) {		
 			for(int col = 0; col < board.boardArray[row].length; col++) {			
 				System.out.print((col != 0) ? " | " + board.boardArray[row][col] : "  " + board.boardArray[row][col]);			
@@ -68,11 +26,5 @@ public class Board {
 			System.out.println((row != 2) ? "\n -----------" : "");
 		}		
 	}
-	
-	
-	
-
-
-
 	
 }
